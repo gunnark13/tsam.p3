8b0833fab453d863e528ee2f692b418f7385ed97
Arnar Kari <arnarkari93@gmail.com>
2015-11-03 21:45:58 +0000

    free gstring

--- valgrind.sh
+++ valgrind.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all src/chatd $(/labs/tsam15/my_port) /home/hir.is/eysteinn13/Tolvusamskipti/tsam15/pa3/src/fd.crt /home/hir.is/eysteinn13/Tolvusamskipti/tsam15/pa3/src/fd.key /home/hir.is/eysteinn13/Tolvusamskipti/tsam15/pa3/src/CAfile.pem
+valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all src/chatd $(/labs/tsam15/my_port) 
