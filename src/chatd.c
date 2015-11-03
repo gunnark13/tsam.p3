5a0ac0b856efeece344ba884ab15987008b59ba1
Arnar Kari <arnarkari93@gmail.com>
2015-11-03 19:42:56 +0000

    let's go this

--- src/chatd.c
+++ src/chatd.c
@@ -82,8 +82,7 @@ void sha256(char *string, char outputBuffer[65])
     SHA256_Update(&sha256, string, strlen(string));
     SHA256_Final(hash, &sha256);
     int i = 0;
-    for(i = 0; i < SHA256_DIGEST_LENGTH; i++)
-    {
+    for(i = 0; i < SHA256_DIGEST_LENGTH; i++) {
         sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
     }
     outputBuffer[64] = 0;
